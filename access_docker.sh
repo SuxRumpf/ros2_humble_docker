28172a3bdc9545ffdd354d161a8a7c690745166e
Moritz Rumpf <moritz.rumpf@alumni.fh-aachen.de>
2024-07-08 16:52:44 +0200

    Anpassungen die noch auf arch getestet werden müssen

--- access_docker.sh
+++ access_docker.sh
@@ -1,6 +1,6 @@
-sudo docker run -it \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    ubuntu_22.04_ros2_humble
+sudo docker run -it --net=host \
+	--env="DISPLAY=$DISPLAY" \
+	ubuntu_22.04_ros2_humble
+    #--env="QT_X11_NO_MITSHM=1" \
+    #--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
 
